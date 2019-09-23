b9750a1e88bbe12d76709ea2e83a660acf56c102
andrsvlz <andrsvlz@gmail.com>
2019-09-23 14:44:52 -0500

    Update ssh.sh

--- src/ssh.sh
+++ src/ssh.sh
@@ -5,6 +5,8 @@ set prompt ":|#|\\\$"
 interact -o -nobuffer -re $prompt return
 send "ansible\r"
 interact -o -nobuffer -re $prompt return
+send "ansible\r"
+interact -o -nobuffer -re $prompt return
 send "ansible-playbook playbooks/linux/notifications.yml -i 10.0.1.189, -u ansible -e 'Session_ID=2019/07/05-28' -e 'site=COP' -e 'Specification=BK_Diario_Pruebaautomatizacion' \r"
 interact -o -nobuffer -re $prompt return
 send "exit\r"

418d2106b7a433edab91715f0bb70601f107170d
andrsvlz <andrsvlz@gmail.com>
2019-09-23 14:45:41 -0500

    Update ssh2.sh

--- src/ssh2.sh
+++ src/ssh2.sh
@@ -5,6 +5,8 @@ set prompt ":|#|\\\$"
 interact -o -nobuffer -re $prompt return
 send "ansible\r"
 interact -o -nobuffer -re $prompt return
+send "ansible\r"
+interact -o -nobuffer -re $prompt return
 send "ansible-playbook /home/ansible/playbooks/linux/cerrejon/cerrejon.yml -i 10.0.1.189,  -u ansible \r"
 interact -o -nobuffer -re $prompt return
 send "exit\r"
